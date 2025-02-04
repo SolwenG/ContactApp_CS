ded84e1deb0e0aabd4d400d6baf27835444040ae
SolwenG <solene.gouin@orange.fr>
2025-02-04 10:04:11 +0100

    initial commit

---
+++ ContactApp/Controllers/ContactController.cs
@@ -0,0 +1,51 @@
+﻿using ContactApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ContactController : Controller
+{
+    private static readonly List<ContactModel> Contacts =
+    [
+        new ContactModel(1, "Solene", "example@gmail.com", "1234567890")
+    ];
+
+    [HttpGet]
+    public IActionResult Index(string? searchValue = null)
+    {
+        var contacts = string.IsNullOrEmpty(searchValue)
+            ? Contacts
+            : Contacts.Where(contact => contact.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+        return View(contacts);
+    }
+
+    [HttpPost]
+    public IActionResult AddContact([FromForm] ContactModel contact)
+    {
+        int Id = Contacts.Count + 1;
+        contact.Id = Id;
+        Contacts.Add(contact);
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    [Route("delete/{id:int}")]
+    public IActionResult RemoveContact(int id)
+    {
+        var existingContacts = Contacts.FirstOrDefault(contact => contact.Id == id);
+        Console.WriteLine(existingContacts);
+        if (existingContacts == null)
+        {
+            return NotFound();
+        }
+
+        Contacts.Remove(existingContacts);
+
+        return RedirectToAction("Index");
+    }
+}
---
+++ ContactApp/Models/ContactModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactApp.Models;
+
+public class ContactModel
+{
+    public int Id { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    
+    [Required, Phone]
+    public string Phone { get; set; }
+
+    public ContactModel() { }
+    
+    public ContactModel(int id, string name, string email, string phone)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Phone = phone;
+    }
+}
